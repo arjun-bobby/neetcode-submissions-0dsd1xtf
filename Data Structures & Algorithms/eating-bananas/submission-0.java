6dc1191a4949cdfd9fefac2a8a49bcd21e3c8f21
arjun-bobby <152247410+arjun-bobby@users.noreply.github.com>
2026-05-16 19:49:33 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+       int l=1,r=0,mk=Integer.MAX_VALUE,k=0,m=0; 
+      for(int i:piles)
+       {
+        r=Math.max(r,i);
+       }
+       while(l<=r)
+       {
+        k=0;
+        m=(l+r)/2;
+        for(int i=0;i<piles.length;i++)
+        {
+            
+            k+=Math.ceil((double)piles[i]/m);
+        }
+        if(k<=h)
+        {
+            mk=m;
+            r=m-1;
+        }
+        else
+        l=m+1;
+
+       }
+        return mk;
+    }
+}
